df52bc94faeaf4c7fa4b6d968ccb01f7553e4670
MercuryRising <solzx007@umn.edu>
2012-11-06 01:31:18 -0600

    Update main.py
    Added loading of comments, tab loading, colorization, ability to access non-self posts, multiple values for multiple posts Not sure if this is the direction you'd like to go with the project, but I got bored and wanted to code something.
    
    Might be interesting for you to look at (although I didn't add any comments)

--- main.py
+++ main.py
@@ -1,23 +1,109 @@
-#! /usr/bin/env python2
-import json, urllib
-url = "http://www.reddit.com/r/all.json"
-all = urllib.urlopen(url).read()
-content = json.loads(all.decode("utf8"))
-y = 1
-stories = {}
 
-for x in content['data']['children']:
-	print "[%d] " % y + x['data']['title']
-	if x['data']['selftext'] != "":
-		print "[Self Text]"
-		selftext = x['data']['selftext']
-		stories[y] = selftext
-	y += 1
-while(1):
-	st = raw_input("Which selfpost would you like to view? > ")
-	if st == "exit":
-		exit()
-	try:
-		print stories[int(st)]
-	except (KeyError, ValueError):
-		print "Sorry, that post does not contain self-text"
+import requests
+import webbrowser
+import time
+
+def print_top(subreddit='all'):
+    url = "http://www.reddit.com/r/"+subreddit+".json"
+
+    r = requests.get(url)
+ 
+    stories = {}
+
+    stories['data'] = r.json['data']['children']
+    print "\nTop %s stories for %s\n" %(len(r.json['data']['children']), colorize(subreddit, "blue"))  
+
+    for index, x in enumerate(r.json["data"]["children"]):
+        data = x['data']
+        index = str(index+1)
+
+        if data['selftext'] != "":
+            print colorize("[%s] " %index, "green"), "%s" % (data['title'].strip())
+            
+            story_data = "\nTitle: " + data['title'] + "\n\n" + data['selftext'] + "\n"
+            stories[index] = story_data
+
+        else:
+            print "[%s]  %s" %(index, data['title'].strip())
+
+    print "\n"
+
+    return stories
+
+color = ['gray', 'red', 'green', 'yellow', 'blue', 'magenta' ,'cyan', 'white', 'crimson']
+colors = {}
+for index, c in enumerate(color):
+    colors[c] = index+30
+
+def load_url(post, kind="comments"):
+    if kind == "comments":
+        url = 'http://www.reddit.com' + post['data'].get('permalink')
+    elif kind == "post":
+        url = post['data'].get('url')
+    webbrowser.open_new_tab(url)
+
+def colorize(text, color='white'):
+    color = colors.get(color, 'white')
+    return '\033[1;%sm%s\033[1;m' %(color, text)
+
+if __name__ == "__main__":
+
+    print "\nThe", colorize("reddit", "red"), "command line loader"
+    print "Enter", colorize("exit", "red"), "to quit"
+    print "Enter a ", colorize("subreddit", "blue"), "to load a specific subreddit"
+    print "Enter blank to reload"
+    print "Enter '10c' to see the comments for the tenth post (any numbers will do)"
+    print "Enter multiple values to load multiple posts in new tabs"
+
+    stories = print_top()
+    subreddit = "all"
+
+    digits = [str(x) for x in range(100)]
+
+    question = "Which " + colorize("self post", "green") + " would you like to view? > "
+
+    while True:
+
+        st = raw_input(question)
+        
+        if st == 'exit':
+            print "Goodbye!"
+            exit()
+
+        if " " in st:
+            sts = st.strip().split(" ")
+            for st in sts:
+                load_url(stories['data'][int(st)-1], kind="post")
+
+
+        elif len(st) == 0:
+            subreddit = st.strip("/r/") if st != "" else subreddit
+            stories = print_top(subreddit)
+
+        elif st[-1] == 'c':
+            if st[0:-1] in digits:
+                prev = st[0:-1]
+                print ""
+                load_url(stories['data'][int(prev)-1], kind="comments")
+
+            elif len(st) > 1:
+                subreddit = st.strip("/r/") if st != "" else subreddit
+
+            time.sleep(0.5)
+            stories = print_top(subreddit)
+
+        elif len(st) > 2:
+            subreddit = st.strip("/r/") if st != "" else subreddit
+            stories = print_top(subreddit)
+
+        elif st[0] in digits if len(st) < 2 else st[0:2]:
+            if stories.get(st):
+                prev = st
+            data = stories.get(st, None)
+            if data == None:
+                load_url(stories['data'][int(st)-1], kind="post")
+                time.sleep(0.5)
+                stories = print_top(subreddit)
+            else:
+                print stories.get(st)
+        time.sleep(0.5)
